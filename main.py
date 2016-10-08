3b8057c8045e6ce48c45a4dab84cd6bcb2c0b225
Stephen <stephen@mathsgrinds.ie>
2016-10-08 21:40:29 +0100

    Update
    Code is a little nicer :P

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import cookielib
 from bs4 import BeautifulSoup
 import json
 
-######################################################################################################
-# FUNCTIONS
-######################################################################################################
-def UstvnowToken(user, password):
+def Ustvnow(user, password):
     cj = cookielib.CookieJar()
     url = "http://m.ustvnow.com/iphone/1/live/login"
     payload = {'username': user, 'password': password, 'device':'gtv', 'redir':'0'}
@@ -14,53 +11,49 @@ def UstvnowToken(user, password):
         r = s.post(url, data=payload)
     html = r.text
     j = json.loads(html)
-    return j['token']
-
-def UstvnowSname(token, station, user, password):
-    cj = cookielib.CookieJar()
-    url = "http://m.ustvnow.com/iphone/1/live/login"
-    payload = {'username': user, 'password': password, 'device':'gtv', 'redir':'0'}
-    with requests.Session() as s:
-        r = s.post(url, data=payload)
-        R = s.get("http://m.ustvnow.com/iphone/1/live/playingnow")
-    html = R.text
-    try:
-        soup = BeautifulSoup(html,"lxml")
-    except:
-        soup = BeautifulSoup(html)
-    for tag in soup.findAll('div'):
-        try:          
-           sname = tag["sname"]
-           if "content" in tag['id']:
-               chunk = html[ html.find(sname) : html.find(sname) + 200 ]
-               chunk = ' '.join(chunk.split())
-               if "<h1>"+station+"</h1>" in chunk:
-                   break
+    token = j['token']
+    m3u8playlist =  "#EXTM3U\n"
+    for station in ["ABC", "CBS", "CW", "FOX", "NBC", "PBS", "My9"]:
+        cj = cookielib.CookieJar()
+        url = "http://m.ustvnow.com/iphone/1/live/login"
+        payload = {'username': user, 'password': password, 'device':'gtv', 'redir':'0'}
+        with requests.Session() as s:
+            r = s.post(url, data=payload)
+            R = s.get("http://m.ustvnow.com/iphone/1/live/playingnow")
+        html = R.text
+        try:
+            soup = BeautifulSoup(html,"lxml")
         except:
-            pass
-    url = "http://m.ustvnow.com/iphone/1/live/viewlive?streamname="+sname+"&stream_origin=ilv10.oedg.ustvnow.com&app_name=ilv10&token="+token+"&passkey=tbd&extrato=tbd"
-    r = s.get(url)
-    html =  r.text
-    try:
-        soup = BeautifulSoup(html,"lxml")
-    except:
-        soup = BeautifulSoup(html)
-    link = "None"
-    for tag in  soup.findAll('video'):
-        link = str(tag['src'])
-        return link
+            soup = BeautifulSoup(html)
+        for tag in soup.findAll('div'):
+            try:          
+               sname = tag["sname"]
+               if "content" in tag['id']:
+                   chunk = html[ html.find(sname) : html.find(sname) + 200 ]
+                   chunk = ' '.join(chunk.split())
+                   if "<h1>"+station+"</h1>" in chunk:
+                       break
+            except:
+                pass
+        url = "http://m.ustvnow.com/iphone/1/live/viewlive?streamname="+sname+"&stream_origin=ilv10.oedg.ustvnow.com&app_name=ilv10&token="+token+"&passkey=tbd&extrato=tbd"
+        r = s.get(url)
+        html =  r.text
+        try:
+            soup = BeautifulSoup(html,"lxml")
+        except:
+            soup = BeautifulSoup(html)
+        link = "None"
+        for tag in  soup.findAll('video'):
+            link = str(tag['src'])
+            m3u8playlist += "#EXTINF:-1," + station + "\n"
+            m3u8playlist += link+ "\n"
+    return m3u8playlist.rstrip("\n")
 
-######################################################################################################
-# LOGIN DETAILS
-######################################################################################################
-username = " Your USTVNOW Username  "
-password = " Your USTVNOW Passoword "
+###########################
+######### MAIN ############
+###########################
 
-######################################################################################################
-# MAIN
-######################################################################################################
-token = UstvnowToken(username, password)
-print "#EXTM3U\n"
-for station in ["ABC", "CBS", "CW", "FOX", "NBC", "PBS", "My9"]:
-    print "#EXTINF:-1," + station
-    print UstvnowSname(token, station, username, password) + "\n"
+# Simply replace with your login details
+
+print Ustvnow("Youur username", "Your password")
+
