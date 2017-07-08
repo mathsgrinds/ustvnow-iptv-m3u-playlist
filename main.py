831a6f7aefdf80e888e8457268fc33648e5e1b33
Stephen <stephen@mathsgrinds.ie>
2017-07-09 00:55:29 +0100

    Update
    CLI Feature Added

--- main.py
+++ main.py
@@ -4,10 +4,25 @@ import cookielib
 import re
 import json
 import os
+import sys
 
 ##################################################################################################################
-username = ""
-password = ""
+try:
+    username = sys.argv[1]
+except:
+    username = "" # Set username manually here if not using the command line
+
+try:
+    password = sys.argv[2]
+except:
+    password = "" # Set username manually here if not using the command line
+
+try:
+    channel = sys.argv[3]
+except:
+    channel = "" # Set channel manually here if not using the command line (use ALL to show all)
+
+CreateSTRM = True
 ##################################################################################################################
  
 def Ustvnow(user, password):
@@ -46,14 +61,13 @@ def Ustvnow(user, password):
         html = r.text
         j = json.loads(html)
         n = 0
-        print "#EXTM3U\n"
+        if channel=="ALL":
+            print "#EXTM3U\n"
         while True:
             # Print Links
             scode = j['results'][n]['scode']
             stream_code = j['results'][n]['stream_code']
             title = j['results'][n]['title']
-            print stream_code
-            print title
             url = "http://m.ustvnow.com/stream/1/live/view?scode="+scode+"&token="+token+"&br_n=Firefox&br_v=54&br_d=desktop"
             r = s.get(url)
             html = r.text
@@ -61,18 +75,23 @@ def Ustvnow(user, password):
                 i = json.loads(html)
             except:
                 break
-            print "#EXTINF:-1,"+stream_code
-            print i["stream"]+"\n"
-            # Create STRM file
-            fname = stream_code+".strm"
-            if os.path.isfile(fname):
-                f= open(fname,"w")
-                f.write(i["stream"])
-                f.close
+            if channel=="ALL":
+                print "#EXTINF:-1,"+stream_code+" - "+title
+                print i["stream"]+"\n"
             else:
-                f= open(fname,"w+")
-                f.write(i["stream"])
-                f.close
+                if channel==stream_code:
+                    print i["stream"].replace("\n","")
+            # Create STRM file
+            if(CreateSTRM):
+                fname = stream_code+".strm"
+                if os.path.isfile(fname):
+                    f= open(fname,"w")
+                    f.write(i["stream"])
+                    f.close
+                else:
+                    f= open(fname,"w+")
+                    f.write(i["stream"])
+                    f.close
             n += 1
         #END 
          
